35d2f3fda111f96e62ae14310b242dd68331b4e9
Mark Chouinard <w0527452@apps.losrios.edu>
2020-03-19 16:32:14 -0700

    Modify build script to include any .md files

--- homework/build.sh
+++ homework/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pandoc -f gfm -t docx --reference-doc=./custom.docx -o ../homework.docx ch7.md
+
+dir="~/CISC324/homework"
+files="";
+for f in "$dir/*.md"; do
+        files+=$(basename "$f")
+
+done
+
+pandoc -f gfm -t docx --reference-doc=./custom.docx -o ../homework.docx $files
